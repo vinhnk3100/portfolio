afdd5072b3c819e9570a98f3314f4880ccbe81bf
Nguyen Vinh <vinhnk2300@gmail.com>
2025-03-07 10:50:58 +0700

    fix: remove the import { ScrollSmoother } from gsap-trial/ScrollSmoother;

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.scss";
 import gsap from "gsap-trial";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
 import CardContent from "./components/CardContent/CardContent";
 import Content from "./components/Content/Content";
 import TopLanding from "./components/TopLanding/TopLanding";
-gsap.registerPlugin(ScrollSmoother);
 
 function App() {
   return (
